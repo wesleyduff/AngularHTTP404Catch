bed3d37a5bc5af9eb3169d6ea49e899aac54790f
Wes Duff <xd@d.com>
2015-02-04 16:35:31 +0000

    update

--- scripts/controllers/mainController.js
+++ scripts/controllers/mainController.js
@@ -5,28 +5,27 @@ This controller is a base controller and is set
 on the <main> DOM tag.
 Use this controller for root like scope that is namespaced away from the global namespace.
 */
-.controller('MainController', ['$scope', 'firebaseFactory', 'OAuthFactory', '$timeout', function($scope, firebaseFactory, OAuthFactory, $timeout){
+.controller('MainController', ['$scope', 'firebaseFactory', 'localstorageservice', '$timeout', function($scope, firebaseFactory, localstorageservice, $timeout){
 	/*remove this when you are ready to start development
 	-also check test/specs/controllerSpecs/boilerplate.js and remove this file. It is there to give you piece of mind when you first clone this repository.
 	*/
 	$scope.user;
+	$scope.hasToken = false;
 	$scope.boilerplate = [
 		"testing to make sure everything is wired up and ready to go!"
 	];
 
-	$scope.loadFacebook = function(){
-		OAuthFactory.loadFacebook(function(){
-			$scope.user = OAuthFactory.user;
-			$scope.$apply();
-		});
-	};
-
 	//setup firebase
 	firebaseFactory.init();
-
-	$scope.$on('facebookLoginClick', function(event, message){
-		$scope.loadFacebook();
-	});
+	
+	
+	$scope.init = function(){
+		//check for oath
+		var token = localstorageservice.deepget().token;
+		if(token) {
+			$scope.hasToken = true;
+		}
+	}
 
 	
 }]);
---
+++ scripts/services/localstorageservice.js
@@ -0,0 +1,81 @@
+(function(angular) {
+    'use strict';
+
+    angular.module("main")
+        .factory('localstorageservice', function() {
+            return {
+                store: {},
+                get: function() {
+                    return this.store;
+                },
+                add: function(obj, fn) {
+
+                    this._store = this.get();
+                    if (typeof fn === 'function') {
+                        fn.apply(this, arguments);
+                    }
+                    for (var prop in obj) {
+                        this.store[prop] = obj[prop];
+                    }
+
+                    return this;
+                },
+                delete: function(obj, fn) {
+                    this._store = this.get();
+                    if (typeof fn === 'function') {
+                        fn.apply(this, arguments[0]);
+                    }
+
+                    for (var prop in obj) {
+                        if (this._store.hasOwnProperty(prop)) {
+                            delete this.store[prop];
+                        }
+                    }
+                },
+                read: function(str, fn) {
+                    if (typeof fn === 'function') {
+                        fn.apply(this, arguments[0]);
+                    }
+                    if (typeof str !== 'string') {
+                        throw "Type Execption: Not a String";
+                    }
+                    if (this.store.hasOwnProperty(str))
+                        return this.store[str];
+                    else
+                        return "not found";
+                },
+                save: function() {
+                    if (typeof window.localStorage === 'object') {
+                        localStorage.removeItem('store');
+                        var lenBefore = localStorage.length, lenAfter;
+
+                        localStorage.setItem('store', JSON.stringify(this.store));
+
+                        lenAfter = localStorage.length;
+
+                        if (lenBefore !== lenAfter) {
+                            return true;
+                        } else {
+                            false;
+                        }
+                    } else {
+                        throw "Browser Not Supported";
+                        return false;
+                    }
+                },
+                deepGet: function() {
+                    if (typeof localStorage === 'object') {
+                        return localStorage.getItem('store') !== null ? JSON.parse(localStorage.getItem('store')) : null;
+                    }
+                },
+                initialize: function() {
+                    var _store = this.deepGet();
+
+                    if (_store !== null) {
+                        delete this.store;
+                        this.store = _store;
+                    }
+                }
+            }
+        });
+})(angular);
